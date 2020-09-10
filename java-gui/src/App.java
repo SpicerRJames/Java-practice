1a30b00931456b6e32d422880044827d99a23faf
SpicerRJames <spicer.r.james@gmail.com>
2020-09-10 16:20:44 -0500

    first java gui

---
+++ java-gui/src/App.java
@@ -0,0 +1,28 @@
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.*;
+
+public class App {
+
+    public App() {
+
+        JFrame frame = new JFrame();
+
+
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0, 1));
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("My Gui");
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+    public static void main(String[] args) {
+        new App();
+    }
+}
+
