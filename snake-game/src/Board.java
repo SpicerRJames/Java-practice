84b0cd05f2e0f0e3741dbab5e53c4f0ba497e238
SpicerRJames <spicer.r.james@gmail.com>
2020-09-10 20:29:57 -0500

    quick update

--- snake-game/src/Board.java
+++ snake-game/src/Board.java
@@ -1,5 +1,21 @@
+import javax.swing.JFrame;
+
 public class Board {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public Board() {
+        Game game = new Game();
+
+        JFrame frame = new JFrame();
+        frame.add(game);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Sanke Game");
+        frame.setLocationRelativeTo(null);
+
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+    public static void main(String[] args) {
+        new Board();
     }
 }
