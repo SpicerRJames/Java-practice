1a2d188a96ecb296ae5123a45a4f2256be023f4a
SpicerRJames <spicer.r.james@gmail.com>
2020-09-15 21:23:47 -0500

    working on simple functionality

--- snake-game/src/Game.java
+++ snake-game/src/Game.java
@@ -2,14 +2,45 @@ import java.awt.Dimension;
 
 import javax.swing.JPanel;
 
-public class Game extends JPanel{
+public class Game extends JPanel implements Runnable{
     
     private static final long serialVersionUID = 1L;
 
     public static final int WIDTH = 500, HEIGHT = 500; 
 
+    private Thread thread;
+    private boolean running;
+
     public Game() {
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
+
+        start();
+    }
+
+    public void start() {
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public void tick() {
+
+    }
+ 
+    public void stop() {
+        running = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+
     }
 
 }
