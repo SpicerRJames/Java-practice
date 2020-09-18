ef90f1c65670cc9f59df6c954c6f738fffa743b5
SpicerRJames <spicer.r.james@gmail.com>
2020-09-17 21:19:02 -0500

    added the black background to the java gui

--- snake-game/src/Game.java
+++ snake-game/src/Game.java
@@ -1,12 +1,17 @@
+import java.awt.Color;
 import java.awt.Dimension;
 
 import javax.swing.JPanel;
 
-public class Game extends JPanel implements Runnable{
-    
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Game extends JPanel implements Runnable, KeyListener {
+
     private static final long serialVersionUID = 1L;
 
-    public static final int WIDTH = 500, HEIGHT = 500; 
+    public static final int WIDTH = 500, HEIGHT = 500;
 
     private Thread thread;
     private boolean running;
@@ -26,7 +31,22 @@ public class Game extends JPanel implements Runnable{
     public void tick() {
 
     }
- 
+
+    public void paint(Graphics g) {
+        g.clearRect(0, 0, WIDTH, HEIGHT);
+
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+
+        for (int i = 0; i < WIDTH / 10; i++) {
+            g.drawLine(i * 10, 0, i * 10, HEIGHT);
+        }
+
+        for (int i = 0; i < HEIGHT / 10; i++) {
+            g.drawLine(i * 10, 0, i * 10, WIDTH);
+        }
+    }
+
     public void stop() {
         running = false;
         try {
@@ -39,11 +59,29 @@ public class Game extends JPanel implements Runnable{
 
     @Override
     public void run() {
-        while(running) {
+        while (running) {
             tick();
             repaint();
 
         }
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
 
     }
 
