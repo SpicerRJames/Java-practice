9df99ec00f9af369c89610b014f9ce8d5203fe5d
SpicerRJames <spicer.r.james@gmail.com>
2020-09-17 21:54:24 -0500

    added BodyPart.java to the project, wip

---
+++ snake-game/src/BodyPart.java
@@ -0,0 +1,24 @@
+import java.awt.Graphics;
+
+public class BodyPart {
+
+    private int xCoor, yCoor, width, height;
+
+    //Constructor for the Body
+    public BodyPart() {
+        this.xCoor = xCoor;
+        this.yCoor = yCoor;
+        width = tileSize;
+        height = tileSize;
+
+    }
+
+    public void tick(){
+
+    }
+
+    public void draw(Graphics g){
+
+    }
+    
+}
