aa9cb7dbd5435819994882747fbbb1eeedafb460
SpicerRJames <spicer.r.james@gmail.com>
2020-10-10 19:47:09 -0500

    update

---
+++ snake-game/src/Apple.java
@@ -0,0 +1,10 @@
+public class Apple {
+    private int xCoor, yCoor, width, height;
+    
+    public Apple(int xCoor, int yCoor, int tileSize) {
+        this.xCoor = xCoor;
+        this.yCoor = yCoor;
+        width = tileSize;
+        height = tileSize;
+    }
+}
