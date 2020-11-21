6dfaaad7a399b0c862ab16a84199d89f67645885
Tipsy <tipsy@pop-os.localdomain>
2020-11-21 17:08:18 -0600

    update

--- demo/src/main/java/Controller/FriendController.java
+++ demo/src/main/java/Controller/FriendController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import Model.Friend;
 import Services.FriendService;
 
 @Controller
@@ -18,5 +19,13 @@ public class FriendController {
         model.addAttribute("listFriends", friendService.getAllFriends());
         return "index";
     }
+
+    @GetMapping("/showNewFriendForm")
+    public String showNewFriendForm(Model model) {
+        Friend friend = new Friend();
+        model.addAttribute("friend", friend);
+        return "new_friend";
+    }
+
 
 }
