65fcc4f885a64a25d16a71a74f141fefedb85ad9
SpicerRJames <spicer.r.james@gmail.com>
2020-11-27 09:19:01 -0600

    friend service update

--- demo/src/main/java/Services/FriendService.java
+++ demo/src/main/java/Services/FriendService.java
@@ -5,5 +5,8 @@ import Model.Friend;
 
 public interface FriendService {
     List<Friend> getAllFriends();
+    void saveFriend(Friend friend);
+    Friend getFriendById(long id);
+    void deleteFriendById(long id);
 }
 //test
